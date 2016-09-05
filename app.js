9c7984692a5f646b0f46576ca3e6e8a026acaf11
Aravind Balla <bsaaravind@gmail.com>
2016-09-06 00:20:38 +0530

    all users

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post('/webhook', function (req, res) {
   var data = req.body;
 
   // Make sure this is a page subscription
-  if (data.object == 'page') {
+  if (0==0) {
     // Iterate over each entry
     // There may be multiple if batched
     data.entry.forEach(function(pageEntry) {
