aae5b47693ce879a3d34841b41ac9df3dbeea6c7
Aravind Balla <bsaaravind@gmail.com>
2016-09-05 21:57:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const
+  vbodyParser = require('body-parser'),
+  config = require('config'),
+  crypto = require('crypto'),
+  express = require('express'),
+  https = require('https'),
+  request = require('request');
+
+var app = express();
+app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'ejs');
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
+
+app.get('/hello', function(req, res){
+  res.send('world!');
+});
