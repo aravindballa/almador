3b84c717b206afc1270a87b41c9401f5d2555ca0
Aravind Balla <bsaaravind@gmail.com>
2016-09-05 22:25:41 +0530

    corrected const error

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-
-var vbodyParser = require('body-parser'),
-var config = require('config'),
-var crypto = require('crypto'),
-var express = require('express'),
-var https = require('https'),
-var request = require('request');
+const
+  bodyParser = require('body-parser'),
+  config = require('config'),
+  crypto = require('crypto'),
+  express = require('express'),
+  https = require('https'),
+  request = require('request');
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
@@ -15,3 +15,26 @@ app.use(express.static('public'));
 app.get('/hello', function(req, res){
   res.send('world!');
 });
+
+
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
