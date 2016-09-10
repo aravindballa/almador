9c03dfbc31afef39432677b1ff496ca915dd4875
Aravind Balla <bsaaravind@gmail.com>
2016-09-10 23:28:40 +0530

    Testing who am I clean code

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ function recievedMessage(event) {
   }
 
   if (message.text === "who am i?") {
-    var urlstr = 'https://graph.facebook.com/v2.6/'+ userId +'?fields=first_name,last_name&access_token=EAAElWHwFC5UBAHUTz3tvUCIicreqS7KDknS5TaOKuZAZCSJwF5zhDE4FF2hFKTU8JGMopwLsWhhC0eKkiMJfeEqdAGfZAZAltSyHO9RXaToS6X5PsDgzGYEaHBln1ScGJT0opPyOKvIj9pZAb1N1cnpnYZAeKwdTAsGmjnuDzeeAZDZD'
+    var urlstr = 'https://graph.facebook.com/v2.6/'+ senderId +'?fields=first_name,last_name&access_token=EAAElWHwFC5UBAHUTz3tvUCIicreqS7KDknS5TaOKuZAZCSJwF5zhDE4FF2hFKTU8JGMopwLsWhhC0eKkiMJfeEqdAGfZAZAltSyHO9RXaToS6X5PsDgzGYEaHBln1ScGJT0opPyOKvIj9pZAb1N1cnpnYZAeKwdTAsGmjnuDzeeAZDZD'
     https.get(urlstr, (res) => {
       res.on('data', (chunk) => {
         console.log(`BODY: ${chunk}`);
